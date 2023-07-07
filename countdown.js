4d3c29573e5e781e8e39532b3f95914204f448da
“shomsato” <satoshoma@watanabemayu.jp>
2023-07-07 10:07:05 +0900

    コードを短く

--- countdown.js
+++ countdown.js
@@ -12,9 +12,9 @@ const rewriteTime = () => {
   const hour = Math.floor(differ / 1000 / 60 / 60);
 
   if (now > kanpaiTime) {
-    elmHour.textContent = String(0);
-    elmMin.textContent = String(0).padStart(2, "0");
-    elmSec.textContent = String(0).padStart(2, "0");
+    elmHour.textContent = "0";
+    elmMin.textContent = "00";
+    elmSec.textContent = "00";
   } else {
     elmHour.textContent = String(hour);
     elmMin.textContent = String(min).padStart(2, "0");
